31f7e2e6bdbcaaa09fc4c2bccb3dfc5a3691fda6
Valencia5 <moses.valencia55@gmail.com>
2022-05-19 10:26:07 -0500

    Java loops

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        //my numb is my variable
+//        int myNum = 5;
+//        //while myNum is less than 15,
+//        while (myNum <= 15) {
+//            //print out myNum is + myNumb
+//            System.out.println("myNum is " + myNum);
+//            //after each time through, increase myNum by one.
+//            myNum++;
+//        }
+//    int myInt = 0;
+//        do {
+//            //start at zero and increase by 2
+//            myInt += 2;
+//            //print out int
+//            System.out.println(myInt);
+//            //while my int is less than 100
+//        } while ( myInt < 100 );
+
+//        for(int i = 5; i <= 15; i++) {
+//            System.out.println("i is " + i);
+//        }
+//        for(int i = 0; i <= 100; i += 2) {
+//            System.out.println("i is " + i);
+//        }
+//        for(int i = 1; i <= 100; i ++) {
+//            if (i % 3 == 0 && i % 5 == 0){
+//                System.out.println("FizzBuzz");
+//            }else if (i % 3 == 0 ){
+//                System.out.println("Fizz");
+//            }else if (i % 5 ==0) {
+//                System.out.println("Buzz");
+//            }else{
+//                System.out.println(i);
+//            }
+//        }
+//        Scanner scn = new Scanner(System.in);
+//        String choice = "y";
+//
+//        while (choice.equalsIgnoreCase("y")){
+//            System.out.println("What number would you like to go up to?");
+//            int userInput = scn.nextInt();
+//            System.out.println("Number | " + "Squared |" + " Cubed");
+//            System.out.println("_______| " + "________|" + "_______");
+////            System.out.println("       |" + "         |" + "      ");
+//
+//            for (int i = 1; i <= userInput; i++){
+//                int numberSquared = (int) Math.pow(i, 2);
+//                int numberCubed = (int) Math.pow(i, 3);
+//                String message ="\n" + i + "      | " + numberSquared + "       | " + numberCubed;
+//
+//                System.out.println(message);
+//
+//
+//            }
+//            System.out.print("Continue? (y/n): " );
+//            choice = scn.next();
+//            System.out.println();
+//        }
+//        Convert given number grades into letter grades.
+//        Prompt the user for a numerical grade from 0 to 100.
+//        Display the corresponding letter grade.
+//        Prompt the user to continue.
+//        Assume that the user will enter valid integers for the grades.
+//        The application should only continue if the user agrees to.
+
+//        String grade = "y";
+//        while(grade.equalsIgnoreCase("y")){
+//            System.out.println("What is your number grade?");
+//            int gradeInput = scn.nextInt();
+//            if( gradeInput >= 88 && gradeInput <= 100){
+//                System.out.println("You received an A.");
+//            } else if( gradeInput >= 80 && gradeInput <= 87){
+//                System.out.println("You received a B. ");
+//            } else if ( gradeInput >= 67 && gradeInput <= 79){
+//                System.out.println("You received a C.");
+//            }  else if ( gradeInput >= 60 && gradeInput <= 66){
+//                System.out.println("You received a D.");
+//            } else if ( gradeInput >= 0 && gradeInput <= 59){
+//                System.out.println("You received an F.");
+//            } else {
+//                System.out.println("That is out of range.");
+//            }
+//            System.out.print("Continue? (y/n): ");
+//            grade = scn.next();
+//            System.out.println();
+//        }
+
+
+    }
+}
