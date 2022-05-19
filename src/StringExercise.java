38d867f1f864c2e0922d7265c63a60211da349a8
Valencia5 <moses.valencia55@gmail.com>
2022-05-19 10:49:56 -0500

    strigs first ex

---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+public class StringExercise {
+    public static void main (String [] args){
+        String edu = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(edu);
+
+        String check= "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(check);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String backslash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(backslash);
+    }
+}
